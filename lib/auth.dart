1da44a7195f4dedba109482eed3d03638e8f804f
Isaac Graves <igraves@hbs.edu>
2019-05-07 10:45:54 -0400

    login

---
+++ lib/auth.dart
@@ -0,0 +1,271 @@
+library muddle.auth;
+
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import './glob.dart' as glob;
+
+final FirebaseAuth auth = FirebaseAuth.instance;
+
+class SignInPage extends StatefulWidget {
+  SignInPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _SignInPageState createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController =
+      TextEditingController(text: glob.loginEmail);
+  final TextEditingController _passwordController =
+      TextEditingController(text: glob.loginPass);
+
+  bool _isLoading = false;
+  bool _login = false;
+
+  var _error;
+
+  @override
+  Widget build(BuildContext context) {
+    final emailField = TextFormField(
+      controller: _emailController,
+      obscureText: false,
+      style: style,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Email",
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'Please enter some text';
+        }
+      },
+    );
+    final passwordField = TextFormField(
+      controller: _passwordController,
+      obscureText: true,
+      style: style,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Password",
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+      validator: (String value) {
+        if (value.isEmpty) {
+          return 'Please enter some text';
+        }
+      },
+    );
+    final loginButton = Material(
+      elevation: 3.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Color(0xff01A0C7),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () async {
+          if (_formKey.currentState.validate()) {
+            setState(() {
+              _isLoading = true;
+            });
+
+            await _signInWithEmailAndPassword();
+
+            print("_login");
+            print(_login);
+
+            setState(() {
+              _isLoading = false;
+            });
+          }
+        },
+        child: Text("Login",
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
+      ),
+    );
+    final createAccountButton = Material(
+      elevation: 3.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Colors.deepOrange,
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () async {
+          if (_formKey.currentState.validate()) {
+            setState(() {
+              _isLoading = true;
+            });
+
+            await _register();
+
+            print("_login");
+            print(_login);
+
+            setState(() {
+              _isLoading = false;
+            });
+          }
+        },
+        child: Text("Create Account",
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
+      ),
+    );
+
+    final credentialsForm = Form(
+      key: _formKey,
+      child: Center(
+        child: Container(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(36.0),
+            child: ListView(shrinkWrap: true, children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(height: 45.0),
+                  emailField,
+                  SizedBox(height: 25.0),
+                  passwordField,
+                  SizedBox(
+                    height: 45.0,
+                  ),
+                  loginButton,
+                  SizedBox(
+                    height: 25.0,
+                  ),
+                  createAccountButton,
+                  SizedBox(
+                    height: 25.0,
+                  ),
+                ],
+              ),
+            ]),
+          ),
+        ),
+      ),
+    );
+
+    Widget _isLoadingWidget() {
+      if (_isLoading) {
+        return (Center(child: CircularProgressIndicator()));
+      }
+      return (credentialsForm);
+    }
+
+    return Scaffold(
+        body: Stack(
+      children: <Widget>[_isLoadingWidget()],
+    ));
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  Future _signInWithEmailAndPassword() async {
+    final FirebaseUser user = await auth
+        .signInWithEmailAndPassword(
+      email: _emailController.text,
+      password: _passwordController.text,
+    )
+        .catchError((error) {
+      print(error);
+      setState(() {
+        _error = error;
+      });
+    });
+
+    if (user != null) {
+      print(user);
+      if (!user.isEmailVerified) user.sendEmailVerification();
+
+      await glob.saveCredentials(
+          _emailController.text, _passwordController.text);
+
+      await glob.userDataFileCheck(user.uid);
+
+      setState(() {
+        _login = true;
+      });
+    } else {
+      setState(() {
+        _login = false;
+      });
+    }
+  }
+
+  Future _register() async {
+    final FirebaseUser user = await auth
+        .createUserWithEmailAndPassword(
+      email: _emailController.text,
+      password: _passwordController.text,
+    )
+        .catchError((error) {
+      print(error);
+      setState(() {
+        _error = error;
+      });
+    });
+
+    if (user != null) {
+      print(user);
+
+      await glob.saveCredentials(
+          _emailController.text, _passwordController.text);
+
+      await glob.save(user.uid, glob.createNewData());
+      await glob.userDataCloudSave(user.uid);
+
+      setState(() {
+        _login = true;
+      });
+    } else {
+      setState(() {
+        _login = false;
+      });
+    }
+  }
+
+  Future<void> loginAlert(message) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: true, // user does not have to tap button
+      builder: (BuildContext context) {
+        return AlertDialog(
+          // title: Text('Message'),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text(message),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('thx'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
---
+++ lib/glob.dart
@@ -0,0 +1,141 @@
+library muddle.glob;
+
+import 'dart:io';
+import 'dart:convert';
+import 'package:dio/dio.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+final secureStore = new FlutterSecureStorage();
+
+String userData = '';
+String loginEmail = '';
+String loginPass = '';
+
+read(filename, old) async {
+  print('READ');
+  try {
+    final directory = await getApplicationDocumentsDirectory();
+    final file = File('${directory.path}/' + filename + '.txt');
+
+    var lastModified = await file.lastModified();
+    var age = DateTime.now().difference(lastModified).inDays;
+
+    if (age > old) return 'old';
+
+    String text = await file.readAsString();
+    return text;
+  } catch (e) {
+    print("Couldn't read file");
+    return 'err';
+  }
+}
+
+save(filename, data) async {
+  print('SAVE');
+  final directory = await getApplicationDocumentsDirectory();
+  final file = File('${directory.path}/' + filename + '.txt');
+  final text = data;
+  await file.writeAsString(text);
+  return true;
+}
+
+saveCredentials(email, pass) async {
+  Map<String, String> allValues = await secureStore.readAll();
+  print(allValues);
+
+  await secureStore.write(key: 'loginEmail', value: email);
+  await secureStore.write(key: 'loginPass', value: pass);
+}
+
+userDataCloudSave(uid) async {
+  final directory = await getApplicationDocumentsDirectory();
+  // final file = File('${directory.path}/' + '12345678' + '.txt');
+
+  final File file = File('${directory.path}/' + uid + '.txt');
+
+  print(file);
+
+  final StorageReference ref =
+      FirebaseStorage.instance.ref().child('userData').child(uid + '.txt');
+  final StorageUploadTask task = ref.putFile(file);
+
+  task.events.listen((event) {
+    if (event.type == StorageTaskEventType.success) {
+      print('UPLOAD FINISHED');
+    }
+  }).onError((error) {
+    print(error);
+  });
+}
+
+userDataFileCheck(uid) async {
+  var checkUserData = await read(uid, -1);
+  if (checkUserData == 'old' || checkUserData == 'err') {
+    final StorageReference ref =
+        FirebaseStorage.instance.ref().child('userData').child(uid + '.txt');
+    String getURL = await ref.getDownloadURL();
+    print(getURL);
+
+    String getData = await getHttp(getURL);
+    if (getData != 'err') {
+      userData = getData;
+      bool localSave = await save(uid, getData);
+      if (localSave) await userDataCloudSave(uid);
+    }
+  } else {
+    userData = checkUserData;
+  }
+}
+
+getHttp(url) async {
+  print('GET - ' + url);
+  try {
+    Response response = await Dio().get(url);
+
+    print('GET success');
+    String data = response.data;
+
+    return data;
+  } catch (e) {
+    print(e);
+    return 'err';
+  }
+}
+
+final Map emptyYear = {
+  "0": {"val": 0, "rec": false, "note": false},
+  "1": {"val": 0, "rec": false, "note": false},
+  "2": {"val": 0, "rec": false, "note": false},
+  "3": {"val": 0, "rec": false, "note": false},
+  "4": {"val": 0, "rec": false, "note": false},
+  "5": {"val": 0, "rec": false, "note": false},
+  "6": {"val": 0, "rec": false, "note": false},
+  "7": {"val": 0, "rec": false, "note": false},
+  "8": {"val": 0, "rec": false, "note": false},
+  "9": {"val": 0, "rec": false, "note": false},
+  "10": {"val": 0, "rec": false, "note": false},
+  "11": {"val": 0, "rec": false, "note": false}
+};
+
+String createNewData() {
+  int thisYear = new DateTime.now().year;
+
+  String data =
+      '{"income":[{"name":"RE Commissions"},{"name":"Other 1"},{"name":"Other 2"}],"expenses":[{"name":"Automobile Expenses","data":[{"name":"Auto Repair"},{"name":"Insurance"},{"name":"Wash and Detail"},{"name":"Gas"},{"name":"Parking - Tolls"},{"name":"DMV and Smog"},{"name":"Auto Lease or Payment"},{"name":"Maintenance"},{"name":"Licensing (annual renewal)"}]},{"name":"Home Office Expense and Deduction","data":[{"name":"Electricity portion of total"},{"name":"Mortgage portion of total"},{"name":"Depreciation on Office Equip"}]},{"name":"Insurance Expenses","data":[{"name":"Disability"},{"name":"Life"},{"name":"Dental"},{"name":"Medical and Health"},{"name":"Vision"}]},{"name":"Office Expenses","data":[{"name":"Assistant and secretarial"},{"name":"Education and Coaching"},{"name":"Entertainment - Meals"},{"name":"Office Supplies"},{"name":"Dues and Subscriptions"},{"name":"Misc"},{"name":"Desk Rental or Office Space"},{"name":"Signs and Materials"},{"name":"MLS Dues"},{"name":"Online Expenses and Marketing"},{"name":"Phone"},{"name":"Internet Charge"},{"name":"Computer"},{"name":"Postage and Freight"},{"name":"Professional Fees"},{"name":"Gifts"},{"name":"Cell Phone"}]},{"name":"Savings Contributions","data":[{"name":"Amount to Reitrement Account"},{"name":"Amount to Savings Account"},{"name":"Amount towards Tax Payments"}]},{"name":"Travel Expenses","data":[{"name":"Meals and Entertainment"},{"name":"Misc Expenses"},{"name":"Air Fare"},{"name":"Car Rental"},{"name":"Hotel Charges"}]}]}';
+
+  var json = jsonDecode(data);
+
+  for (var type in json['income']) {
+    type['data'] = {thisYear.toString(): emptyYear};
+  }
+  for (var type in json['expenses']) {
+    for (var item in type['data']) {
+      item['data'] = {thisYear.toString(): emptyYear};
+    }
+  }
+
+  return jsonEncode(json);
+}
